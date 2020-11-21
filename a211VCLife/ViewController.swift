f9eec3da8853008ade82dfe491ba0a5d77e83dda
Shen Jun Wu <shenfive@gmail.com>
2020-11-21 11:46:31 +0800

    rotate

--- a211VCLife/ViewController.swift
+++ a211VCLife/ViewController.swift
@@ -9,16 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var counter = 0
+    var counter:Double = 0
     var timer:Timer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
-        
-
-        
     }
     
     
@@ -31,9 +28,26 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear")
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showCounter), userInfo: nil, repeats: true)
-        
+        let newView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        newView.center = self.view.center
+        newView.backgroundColor = UIColor.yellow
+        newView.transform = CGAffineTransform(rotationAngle: 0)
+        view.addSubview(newView)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (timer) in
+            self.rotateView(targateView: newView)
+        })
+
     }
+    
+    func rotateView(targateView:UIView){
+        let angle =  counter * .pi / 180
+        targateView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        counter += 4
+        print(counter)
+    }
+
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewDidAppear")
